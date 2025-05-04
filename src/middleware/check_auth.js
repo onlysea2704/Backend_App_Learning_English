093cd0c643942382327d9a15ff654bb4f4f7101b
onlysea2704 <phamduyhai2704@gmail.com>
2025-05-04 09:35:12 +0700

    m

--- src/middleware/check_auth.js
+++ src/middleware/check_auth.js
@@ -3,50 +3,54 @@ import db from "../models";
 
 export const checkAdminRole = (requiredRole) => async (req, res, next) => {
 
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized: Token missing' });
+  }
+
   const token = req.headers.authorization.split(" ")[1]
-      try {
-        // Xác thực ID Token bằng Firebase Admin SDK
-        const decodedToken = await admin.auth().verifyIdToken(token);
-        const uid = decodedToken.uid;
-    
-        // Lấy user từ database
-        const [user] = await db.findOne()
+  try {
+    // Xác thực ID Token bằng Firebase Admin SDK
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const uid = decodedToken.uid;
 
-        console.log(user)
-        if (!user) return res.status(404).send("User not found");
+    // Lấy user từ database
+    const [user] = await db.findOne({ where: { firebase_user_id: uid } })
 
-        if (user[0].role !== requiredRole) {
-          return res.status(403).send("Forbidden");
-        }
+    console.log(user)
+    if (!user) return res.status(404).send("User not found");
 
-        req.user = user;
-        next();
-      } catch (error) {
-        res.status(401).send("Unauthorized");
-      }
+    if (user[0].role !== requiredRole) {
+      return res.status(403).send("Forbidden");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send("Unauthorized");
+  }
 };
 
 export const checkUser = (requiredRole) => async (req, res, next) => {
 
-    const token = req.headers.authorization.split(" ")[1]
-        try {
-          // Xác thực ID Token bằng Firebase Admin SDK
-          const decodedToken = await admin.auth().verifyIdToken(token);
-          const uid = decodedToken.uid;
-      
-          // Lấy user từ database
-          const [user] = await db.findOne()
-  
-          console.log(user)
-          if (!user) return res.status(404).send("User not found");
-  
-          if (user[0].role !== requiredRole) {
-            return res.status(403).send("Forbidden");
-          }
-  
-          req.user = user;
-          next();
-        } catch (error) {
-          res.status(401).send("Unauthorized");
-        }
-  };
+  const token = req.headers.authorization.split(" ")[1]
+  try {
+    // Xác thực ID Token bằng Firebase Admin SDK
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const uid = decodedToken.uid;
+
+    // Lấy user từ database
+    const [user] = await db.findOne()
+
+    console.log(user)
+    if (!user) return res.status(404).send("User not found");
+
+    if (user[0].role !== requiredRole) {
+      return res.status(403).send("Forbidden");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send("Unauthorized");
+  }
+};
