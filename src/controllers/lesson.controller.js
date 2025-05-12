f0d4c61f9a9faed93895f0b9cf72ad15c3cd7423
onlysea2704 <phamduyhai2704@gmail.com>
2025-05-12 13:09:57 +0700

    m

--- src/controllers/lesson.controller.js
+++ src/controllers/lesson.controller.js
@@ -1,18 +1,41 @@
 import db from "../models/index.js";
 
-export const getAllLessonById = async (req, res) => {
+export const getAllLessonByIdCourse = async (req, res) => {
 
-    // input {idCourse}
-    const idCourse = req.body.idCourse;
-    const lessons = await db.Lesson.findAll({ where: { id_course: idCourse } })
-    lessons.map((lesson) => {
-        if(lesson.type_lesson){
-            
-        }else {
+    try {
+        // input {idCourse}
+        const idCourse = req.body.idCourse;
+        // const lessons = await db.Lesson.findAll({ where: { id_course: idCourse } })
+        // lessons.map((lesson) => {
+        //     if(lesson.type_lesson){
 
-        }
-    })
-    return res.json(lessons)
+        //     }else {
+
+        //     }
+        // })
+        const results = await db.sequelize.query(`
+        SELECT 
+            l.id_lesson,
+            l.id_course,
+            l.order_lesson,
+            l.type_lesson,
+            q.name_quiz,
+            le.name_lecture
+        FROM lessons l
+        LEFT JOIN quizes q ON l.id_lesson = q.id_lesson
+        LEFT JOIN lectures le ON l.id_lesson = le.id_lesson
+        WHERE l.id_course = :id_course
+        ORDER BY l.order_lesson ASC
+        `, {
+            replacements: { id_course: idCourse },
+            type: db.sequelize.QueryTypes.SELECT
+        })
+        console.log(results[0].name_quiz)
+        return res.json(results[0].name_quiz)
+    } catch (error) {
+        console.log('error: ', error.message)
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 export const getLectureById = async (req, res) => {
