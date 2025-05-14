3a7160e814661f8c8b9a185fdab16acce773f946
onlysea2704 <phamduyhai2704@gmail.com>
2025-05-14 18:30:24 +0700

    m

--- src/controllers/result.controller.js
+++ src/controllers/result.controller.js
@@ -1,15 +1,29 @@
 import db from "../models/index.js";
 
-export const getResultByIdLesson = async (req, res) => {
+export const getResulResponsetByIdLesson = async (req, res) => {
     try {
         const idUser = req.user.id_user;
         const idLesson = req.body.idLesson;
         const student = await db.Student.findOne({ where: { id_user: idUser } });
         const quiz = await db.Quiz.findOne({ where: { id_lesson: idLesson } });
         const result = await db.Result.findOne({ where: { id_student: student.id_student, id_quiz: quiz.id_quiz } });
-        console.log('result: ', result);
-        return res.json(result);
+        const questions = await db.Question.findAll({ where: { id_quiz: quiz.id_quiz }, raw: true, });
+        const responses = await db.Response.findAll({ where: { id_result: result.id_result, id_student: student.id_student }, raw: true, });
+        const newFormatQuestions = questions.map(q => ({
+            ...q,
+            options: [q.option_1, q.option_2, q.option_3, q.option_4]
+        }));
+
+        const responseQuestions = newFormatQuestions.map(question => {
+            const response = responses.find(r => r.id_question === question.id_question);
+            return {
+                ...question,
+                response: response ? response : null
+            };
+        });
+        return res.json(responseQuestions);
     } catch (error) {
+        console.log('error: ', error.message);
         return res.json();
     }
 }
--- src/routes/result.router.js
+++ src/routes/result.router.js
@@ -1,9 +1,9 @@
 import express from "express"
 import { checkAdminRole, checkUser } from "../middleware/check_auth.js"
 import multer from "multer"
-import { getResultByIdLesson } from "../controllers/result.controller.js"
+import { getResulResponsetByIdLesson } from "../controllers/result.controller.js"
 
 const router = express.Router()
-router.post('/get-result-by-id-lesson', checkUser, getResultByIdLesson)
+router.post('/get-result-by-id-lesson', checkUser, getResulResponsetByIdLesson)
 
 export default router;
