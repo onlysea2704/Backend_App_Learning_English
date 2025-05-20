0b707d30e37b73e44a9ed4e61f3b78c885d27af3
onlysea2704 <phamduyhai2704@gmail.com>
2025-05-20 14:44:31 +0700

    m

--- src/seed-data/seed-user.js
+++ src/seed-data/seed-user.js
@@ -11,6 +11,7 @@ export const seedUsers = async () => {
         firebaseUserIds.map((firebaseUserId) => {
             db.User.create({ firebase_user_id: firebaseUserId, role: 'user' })
         })
+        await db.User.update({ role: 'admin' }, { where: { firebase_user_id: 'UiHakuBQUAPwjkwbFug0NHZmzMT2' } })
         console.log("✅ Đã seed dữ liệu vào bảng user")
     } catch (error) {
 
